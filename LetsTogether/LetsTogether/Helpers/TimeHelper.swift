833cfa03d525577c474e759695485e03f5d91d07
Mateus <mateusnobref@gmail.com>
2021-09-15 12:03:24 -0300

    Classe de manipulacao de cronometro criada

--- LetsTogether/LetsTogether/Helpers/TimeHelper.swift
+++ LetsTogether/LetsTogether/Helpers/TimeHelper.swift
@@ -10,11 +10,16 @@ import Foundation
 class TimeHelper {
     
     var timer = Timer()
+    var secondsCounter = 0
     
     func startSecondsTimer(escaping action: @escaping (TimeInterval) -> Void) {
         timer = Timer.init(timeInterval: 1, repeats: true) {t in
+            self.secondsCounter += 1
             action(t.timeInterval)
         }
     }
     
+    func secondsToHourMinutesAndSeconds() -> (Int, Int, Int) {
+        return (secondsCounter/3600, (secondsCounter%3600), (secondsCounter%3600) )
+    }
 }
